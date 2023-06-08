23058f845c85a9c0c676cfa89ed55497dce847bc
GalaxyGamingBoy <xrteach@gmail.com>
2023-06-08 20:08:31 +0000

    feat: Vercel Support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://HACK-SP-BR.github.io',
-    output: 'server'
+  site: 'https://HACK-SP-BR.github.io',
+  output: 'server',
+  adapter: vercel()
 });
